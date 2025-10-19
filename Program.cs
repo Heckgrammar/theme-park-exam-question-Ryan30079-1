59980f1513507ff6df112b4a63a6a796bc7c3a96
Ryan (Fruit Army) <22rpallapati@heckgrammar.co.uk>
2025-10-19 12:32:32 +0100

    .
    .

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using System.Security.Principal;
+using System.Xml;
+
+namespace LinearSearchFruitsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int charge = 15;
+            Console.WriteLine("Enter number of people in the group");
+            int people = Convert.ToInt32(Console.ReadLine());
+            if (people >= 6)
+            {
+                int total = (charge * people) - 15;
+                Console.WriteLine("Your total is £" + total);
+            }
+            else
+            {
+                int total = charge * people;
+                Console.WriteLine("Your total is £" + total);
+            }
+
+        }
+    }
+}
